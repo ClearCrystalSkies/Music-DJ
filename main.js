19b2cc33ce66018d4af1ba56261a6c03b70c0eaa
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2023-05-15 12:37:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var peterpansong = "";
 var music2 = "";
+var leftWristX =0;
+var rightWristX =0;
+var leftWristY =0;
+var righttWristY =0;
 function preload(){
     peterpansong = loadSound("music.mp3");
     music2 = loadSound("music2.mp3");
@@ -10,7 +14,21 @@ function setup(){
     console.log("Canvas has loaded.");
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 };
 function draw(){
     image(video, 0 , 0, 600 ,500);
 };
+function modelLoaded(){
+    console.log("Model has been loaded!!");
+}
+function gotPoses(results){
+    if(results > 0){
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        righttWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX:" + leftWristX + ", leftWristY:" + leftWristY + ", rightWristX:" + rightWristX + ", rightWristY:" + righttWristY);
+    }
+}
