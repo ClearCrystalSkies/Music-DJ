6208d3e9b8606d340ebb96be795041cf3d8850d8
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2023-05-16 13:25:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 var peterpansong = "";
-var music2 = "";
+var hptheme = "";
 var leftWristX =0;
 var rightWristX =0;
 var leftWristY =0;
 var righttWristY =0;
+var ppStatus = "";
+var hpStatus = "";
+var leftWristScore = 0;
+var rightWristScore = 0;
 function preload(){
     peterpansong = loadSound("music.mp3");
-    music2 = loadSound("music2.mp3");
+    hptheme = loadSound("music2.mp3");
 };
 function setup(){
     canvas = createCanvas(600, 500);
@@ -19,16 +23,39 @@ function setup(){
 };
 function draw(){
     image(video, 0 , 0, 600 ,500);
+    fill("red");
+    stroke("red");
+    ppStatus = peterpansong.isPlaying();
+    hpStatus = hptheme.isPlaying();
+    if (leftWristScore > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        if(ppStatus = false){
+            peterpansong.play();
+            document.getElementById("song_name").innerHTML ="Now playing: Peter pan song";
+            hptheme.stop();
+        }
+    }
+    if (rightWristScore > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        if(hpStatus = false){
+            hptheme.play();
+            document.getElementById("song_name").innerHTML ="Now playing: Harry Potter Theme song";
+            peterpansong.stop();
+        }
+    }
 };
 function modelLoaded(){
-    console.log("Model has been loaded!!");
+    console.log("PoseNet has been loaded!!");
 }
 function gotPoses(results){
-    if(results > 0){
+    if(results.length > 0){
+        console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         rightWristX = results[0].pose.rightWrist.x;
         righttWristY = results[0].pose.leftWrist.y;
         console.log("LeftWristX:" + leftWristX + ", leftWristY:" + leftWristY + ", rightWristX:" + rightWristX + ", rightWristY:" + righttWristY);
+        leftWristScore = results[0].pose.keypoints[9].score;
+        rightWristScore = results[0].pose.keypoints[10].score;
     }
 }
